98982bb0d3c85e3b10f48f751f914230f7432f75
VuBacktracking <vhoang9999@gmail.com>
2025-05-21 09:22:52 +0700

    Add utils for datawarehouse

---
+++ utils/create_schema.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from dotenv import load_dotenv
+from postgresql_client import PostgresSQLClient
+from pathlib import Path
+
+# Load environment variables from the .env file
+dotenv_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(".env")
+
+def main():
+    # Initialize the PostgresSQLClient with connection parameters
+    try:
+        pc = PostgresSQLClient(
+            database=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        )
+        logging.info("PostgresSQLClient initialized successfully.")
+    except Exception as e:
+        logging.error(f"Failed to initialize PostgresSQLClient: {e}")
+        return
+
+    # SQL statements to create schemas
+    create_staging_schema = """
+                                DROP SCHEMA IF EXISTS staging;
+                                CREATE SCHEMA IF NOT EXISTS staging;
+                                """
+
+    # Execute schema creation queries with error handling
+    try:
+        logging.info("Creating 'staging' schema...")
+        pc.execute_query(create_staging_schema)
+        logging.info("'staging' schema created successfully.")
+        
+    except Exception as e:
+        logging.error(f"Failed to create schema: {e}")
+        print(f"Failed to create schema with error: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ utils/create_table.py
@@ -0,0 +1,65 @@
+
+import os
+import logging
+from dotenv import load_dotenv
+from postgresql_client import PostgresSQLClient
+from pathlib import Path
+
+# Load environment variables
+dotenv_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(".env")
+
+def main():
+    # Initialize PostgresSQLClient with credentials from environment variables
+    try:
+        pc = PostgresSQLClient(
+            database=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        )
+        logging.info("PostgreSQL client initialized successfully.")
+    except Exception as e:
+        logging.error(f"Failed to initialize PostgreSQL client: {e}")
+        return
+
+    # SQL queries to create tables
+    create_table_staging = """
+        DROP TABLE IF EXISTS staging.nyc_taxi;
+        CREATE TABLE IF NOT EXISTS staging.nyc_taxi (
+            year                    VARCHAR,
+            month                   VARCHAR,
+            dow                     VARCHAR,
+            vendor_id               INT, 
+            rate_code_id            FLOAT, 
+            pickup_location_id      INT, 
+            dropoff_location_id     INT, 
+            payment_type_id         INT, 
+            pickup_datetime         TIMESTAMP WITHOUT TIME ZONE, 
+            dropoff_datetime        TIMESTAMP WITHOUT TIME ZONE, 
+            pickup_latitude         FLOAT,
+            pickup_longitude        FLOAT,
+            dropoff_latitude        FLOAT,
+            dropoff_longitude       FLOAT,
+            passenger_count         FLOAT, 
+            trip_distance           FLOAT,
+            extra                   FLOAT, 
+            mta_tax                 FLOAT, 
+            fare_amount             FLOAT, 
+            tip_amount              FLOAT, 
+            tolls_amount            FLOAT, 
+            total_amount            FLOAT, 
+            improvement_surcharge   FLOAT, 
+            congestion_surcharge    FLOAT,
+            service_type            INT,
+        );
+    """
+
+    # Execute queries with error handling
+    try:
+        pc.execute_query(create_table_staging)
+        logging.info("Table 'staging.nyc_taxi' created successfully or already exists.")
+    except Exception as e:
+        logging.error(f"Failed to create table 'staging.nyc_taxi': {e}")
+
+if __name__ == "__main__":
+    main()
